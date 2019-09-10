6ee267f7d1474b2cad5885bcaf75c52c4409191d
Wang Xing <wangstar1031@hotmail.com>
2019-09-11 03:11:35 +0800

    Updated for changed linkedin format.

--- background.js
+++ background.js
@@ -1,12 +1,12 @@
-chrome.runtime.onInstalled.addListener(function() {
-	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-		chrome.declarativeContent.onPageChanged.addRules([{
-			conditions: [new chrome.declarativeContent.PageStateMatcher({
-				pageUrl: {hostEquals: 'www.linkedin.com'},
-				})
-			],
-			actions: [new chrome.declarativeContent.ShowPageAction()]
-		}]);
-	});
-});
+// chrome.runtime.onInstalled.addListener(function() {
+// 	chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
+// 		chrome.declarativeContent.onPageChanged.addRules([{
+// 			conditions: [new chrome.declarativeContent.PageStateMatcher({
+// 				pageUrl: {hostEquals: 'www.linkedin.com'},
+// 				})
+// 			],
+// 			actions: [new chrome.declarativeContent.ShowPageAction()]
+// 		}]);
+// 	});
+// });
 
--- scrap.js
+++ scrap.js
@@ -6,22 +6,38 @@ if( docmentUrl.indexOf("linkedin.com/in/") == -1) {
 }
 else {
 	// chrome.storage.sync.set({pageError: "pageOK"});
-	var strName = document.getElementsByClassName("pv-top-card-section__name")[0].innerHTML;
+
+    var strName = $(".pv-top-card-v3 ul.pv-top-card-v3--list li").eq(0).text().trim();
+    if( !strName ){
+        strName = $("h1.pv-top-card-section__name").text().trim();
+    }
+	// var strName = document.getElementsByClassName("pv-top-card-section__name")[0].innerHTML;
 	chrome.storage.sync.set({strName: strName});
 
-	var HeadLineCtrl = document.getElementsByClassName("pv-top-card-section__headline")[0];
-	var strHeadLine = "";
-	if( HeadLineCtrl){
-		strHeadLine = HeadLineCtrl.innerHTML;
-	}
+
+    var strHeadLine = $(".pv-top-card-v3 h2").eq(0).text().trim();
+    if( !strHeadLine){
+        strHeadLine = $("h2.pv-top-card-section__headline").text().trim();
+    }
+	// var HeadLineCtrl = document.getElementsByClassName("pv-top-card-section__headline")[0];
+	// var strHeadLine = "";
+	// if( HeadLineCtrl){
+	// 	strHeadLine = HeadLineCtrl.innerHTML;
+	// }
 	chrome.storage.sync.set({strHeadLine: strHeadLine});
 
-	var LocationCtrl = document.getElementsByClassName("pv-top-card-section__location")[0];
-	var strLocation = "";
-	if( LocationCtrl){
-		strLocation = LocationCtrl.innerHTML;
-	}
+
+    var strLocation = $("ul.pv-top-card-v3--list-bullet li").eq(0).text().trim();
+    if( !strLocation ){
+        strLocation = $("h3.pv-top-card-section__location").text().trim();
+    }
+	// var LocationCtrl = document.getElementsByClassName("pv-top-card-section__location")[0];
+	// var strLocation = "";
+	// if( LocationCtrl){
+	// 	strLocation = LocationCtrl.innerHTML;
+	// }
 	chrome.storage.sync.set({strLocation: strLocation});
+
 	var isExperienceScrapped = false;
 	var expMoreButton = $("#experience-section").find("button.pv-profile-section__see-more-inline");
 	if( expMoreButton.length && $("#experience-section").find("button.pv-profile-section__see-more-inline").attr("aria-expanded") == "false"){
@@ -62,6 +78,7 @@ else {
 					var strExperience = JSON.stringify(lstExperience);
 					chrome.storage.sync.set({strExperience:strExperience});
 					isExperienceScrapped = true;
+					console.log(strExperience);
 					console.log("Experience scrapping finished.");
 				}, 100);
 			}
@@ -122,6 +139,7 @@ else {
 					chrome.storage.sync.set({strEducation:strEducation});
 					isEducationScrapped = true;
 					console.log("Education scrapping finished");
+					console.log(strEducation);
 				}, 100);
 			}
 		}, 1000);
@@ -143,21 +161,24 @@ else {
 		isEducationScrapped = true;
 	}
 
-	var strImgStyle = $(".pv-top-card-section__photo").attr("style");
-	chrome.storage.sync.set({strImgUrl: ""});
-	var arrStyles = strImgStyle.split(";");
-	for( var i = 0; i < arrStyles.length; i++){
-		var strStyle = arrStyles[i];
-		var arrOneStyle = strStyle.split(":");
-		if( arrOneStyle[0] == 'background-image'){
-			arrOneStyle.shift();
-			var strUrlFunc = arrOneStyle.join(':').trim();
-			var strImgUrl = strUrlFunc.replace('url("', "");
-			strImgUrl = strImgUrl.replace('")', "");
-			chrome.storage.sync.set({strImgUrl: strImgUrl});
-			break;
-		}
-	}
+	var strImgUrl = $("img.pv-top-card-section__photo")[0].src;
+	chrome.storage.sync.set({strImgUrl: strImgUrl});
+
+	// var strImgStyle = $(".pv-top-card-section__photo").attr("style");
+	// chrome.storage.sync.set({strImgUrl: ""});
+	// var arrStyles = strImgStyle.split(";");
+	// for( var i = 0; i < arrStyles.length; i++){
+	// 	var strStyle = arrStyles[i];
+	// 	var arrOneStyle = strStyle.split(":");
+	// 	if( arrOneStyle[0] == 'background-image'){
+	// 		arrOneStyle.shift();
+	// 		var strUrlFunc = arrOneStyle.join(':').trim();
+	// 		var strImgUrl = strUrlFunc.replace('url("', "");
+	// 		strImgUrl = strImgUrl.replace('")', "");
+	// 		chrome.storage.sync.set({strImgUrl: strImgUrl});
+	// 		break;
+	// 	}
+	// }
 	var enInfo = $(".pv-top-card-v2-section__link--contact-info");
 	var otherInfo = $(".contact-see-more-less");
 	if( enInfo.length != 0){
@@ -168,7 +189,18 @@ else {
 	var isProfileScrapped = false;
 	var profileInt = setInterval(function(){
 		if( isExperienceScrapped == true && isEducationScrapped == true ){
-			document.getElementsByClassName("pv-top-card-v2-section__link--contact-info")[0].click();
+			console.log("exp edu scrapping finished.");
+			// document.getElementsByClassName("pv-top-card-v2-section__link--contact-info")[0].click();
+			// var curUrl = document.location.href;
+			// if( curUrl.substr(curUrl.length - 1) == "/"){
+			// 	document.location.href = curUrl + "detail/contact-info/";
+			// } else{
+			// 	document.location.href = curUrl + "/detail/contact-info/";
+			// }
+			setTimeout(function(){
+				document.querySelectorAll("a[data-control-name='contact_see_more']")[0].click();
+				// $("a[data-control-name=contact_see_more]").click();
+			})
 			clearInterval(profileInt);
 		}
 	}, 100);
@@ -181,7 +213,7 @@ else {
 		chrome.storage.sync.set({strProfile: strProfile});
 
 		var EmailCtrl = document.getElementsByClassName("ci-email")[0]; 
-		var strEmail = ""; 
+		var strEmail = "";
 		if( EmailCtrl){ 
 			strEmail= EmailCtrl.getElementsByTagName("a")[0].innerHTML;
 		}
@@ -208,7 +240,8 @@ else {
 		}
 		chrome.storage.sync.set({strSite: strSite});
 
-		document.getElementsByClassName("artdeco-dismiss")[0].click();
+		// document.getElementsByClassName("artdeco-dismiss")[0].click();
+		document.getElementsByClassName("artdeco-modal__dismiss")[0].click();
 		isProfileScrapped = true;
 		
 	}, 50);
